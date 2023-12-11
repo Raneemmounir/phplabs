2ddabbdc44b0a40f783ba3091a4af763e48e59d5
raneem <raneemhegy479@gmail.com>
2023-12-11 16:15:56 +0200

    day2

---
+++ day2/data.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$info =  [
+    "First Name" => $_POST['fn'],
+    "Last Name" => $_POST['ln'],
+    "Address" => $_POST['msg'],
+    "Contry" => $_POST['select'],
+    "Gender" => $_POST['F'],
+
+
+];
+$Skills = $_POST['skills'];
+echo "<table border='1'>";
+foreach ($info as $key => $value) {
+    echo "<tr>";
+
+    echo "<td>$key</td>";
+    echo "<td>$value</td>";
+    echo "</tr>";
+}
+echo "<tr><td > skills</td><td>";
+foreach($Skills as $Skill){
+    echo $Skill." - ";
+}
+echo "</td></tr>";
+echo "</table>";
